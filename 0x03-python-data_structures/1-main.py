755aa4222663053e9f5abca8130f387fda76d4b5
simlexx-k <simonwriter254@gmail.com>
2024-03-30 02:06:20 -0700

    update all file permissions

--- 0x03-python-data_structures/1-main.py
+++ 0x03-python-data_structures/1-main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-from `1-element_at` import element_at
-
+element_at = __import__('1-element_at').element_at
 my_list = [1, 2, 3, 4, 5]
 idx = 3
 print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
