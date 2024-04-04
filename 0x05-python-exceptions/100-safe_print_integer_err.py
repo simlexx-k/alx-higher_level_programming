83e1d082a3f37743ed2ae80eb0d889283f50c982
simlexx-k <simonwriter254@gmail.com>
2024-04-04 08:35:42 -0700

    Safe Print Integer Error

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+import sys
+
+
+def safe_print_integer_err(value):
+    """Prints an integer with error handling."""
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        error_msg = "Exception:Unknown format code 'd' for object of type '{}'"
+        error_msg = error_msg.format(type(value).__name__)
+        print(error_msg, file=sys.stderr)
+        return False
+
+
+if __name__ == "__main__":
+    value = 89
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = -89
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = "School"
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
