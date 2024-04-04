7c9b19729f273886a540b5795b27429fc6a6f57e
simlexx-k <simonwriter254@gmail.com>
2024-04-04 09:09:07 -0700

    CPython runner

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,22 +1,21 @@
 #include <Python.h>
 
-
 /* Function to print information about Python lists */
 void print_python_list(PyObject *p) {
     Py_ssize_t size, i;
-
 
     if (!PyList_Check(p)) {
         fprintf(stderr, "[*] Python list info\n  [ERROR] Invalid List Object\n");
         return;
     }
 
-    size = PyList_Size(p);
+    size = PyObject_Length(p);
     printf("[*] Python list info\n[*] Size of the Python List = %ld\n", size);
     printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
     for (i = 0; i < size; i++) {
-        printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+        PyObject *item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
     }
 }
 
@@ -25,13 +24,12 @@ void print_python_bytes(PyObject *p) {
     Py_ssize_t size, i;
     char *str;
 
-
     if (!PyBytes_Check(p)) {
         fprintf(stderr, "[.] bytes object info\n  [ERROR] Invalid Bytes Object\n");
         return;
     }
 
-    size = PyBytes_Size(p);
+    size = PyObject_Size(p);
     printf("[.] bytes object info\n  size: %ld\n", size);
 
     str = PyBytes_AsString(p);
@@ -46,7 +44,6 @@ void print_python_bytes(PyObject *p) {
 /* Function to print information about Python float objects */
 void print_python_float(PyObject *p) {
     double val;
-
 
     if (!PyFloat_Check(p)) {
         fprintf(stderr, "[.] float object info\n  [ERROR] Invalid Float Object\n");
